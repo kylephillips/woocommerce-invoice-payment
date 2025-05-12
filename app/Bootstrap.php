6f04a5d7dfcbc869b1712d6a663287c1ef1612b1
Kyle Phillips <kylephillipsdesign@gmail.com>
2025-05-12 13:53:46 -0400

    Updates plugin version

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -22,7 +22,7 @@ class Bootstrap
 	{
 		$plugin_directory = plugins_url() . '/' . basename(dirname(dirname(__FILE__)));
 		define('WOOINVOICEPAYMENT_PLUGIN_DIRECTORY', $plugin_directory);
-		define('WOOINVOICEPAYMENT_VERSION', '1.0.3');
+		define('WOOINVOICEPAYMENT_VERSION', '1.0.4');
 		define('WOOINVOICEPAYMENT_DOMAIN', 'woocommerce-invoice-payment'); // Localization domain
 	}
 
